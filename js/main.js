b9ff6d5e38f1098a04260f2f871bd3e0201c4863
Bo Chean <bchean@gmail.com>
2015-10-08 01:37:42 -0700

    Fix https in CDN url. Dashboard UI tweaks.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ var jqXHR = $.ajax({
     url: 'api/videos',
     dataType: 'json'
 }).done(function(data) {
+    $('.loading').remove();
     data.sort(function(a, b) {
         return b.numImpressions - a.numImpressions;
     });
     for (var i = 0; i < data.length; i++) {
-        $('.dashboard').append('<div class="video">' + JSON.stringify(data[i]) + '</div>');
+        var video = data[i];
+        $('.dashboard').append('<div class="video">[' + video.numImpressions + '] <a href="https://www.youtube.com/watch?v=' + video.youtubeId + '">' + video.title + '</a></div>');
     }
 }).fail(function(jqXHR, textStatus, errorThrown) {
     $('.dashboard').val(errorThrown);
