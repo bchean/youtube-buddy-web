4a335d93565315f87dc05814fe79a5fb395b2a3d
Bo Chean <bchean@gmail.com>
2015-11-16 01:07:31 -0800

    Add one-week stats to dashboard.

--- js/load-pings.js
+++ js/load-pings.js
@@ -3,26 +3,32 @@ var now_ms = Date.now();
 var oneDayAgo = new Date(now_ms - MS_PER_DAY).toISOString();
 var oneWeekAgo = new Date(now_ms - 7*MS_PER_DAY).toISOString();
 
-$.ajax({
-    method: 'GET',
-    url: 'api/pings',
-    data: {sinceDateTime: oneDayAgo},
-    dataType: 'json'
-}).done(function(videoPingInfoList) {
-    $('.pings-loading').remove();
-    if (videoPingInfoList.length) {
-        for (var i = 0; i < videoPingInfoList.length; i++) {
-            var videoPingInfo = videoPingInfoList[i];
-            $('.pings-data').append(
-                '<div class="ping">' +
-                '[' + videoPingInfo.numPings + '] ' +
-                '<a href="https://www.youtube.com/watch?v=' + videoPingInfo.youtubeId + '" target="_blank">' +
-                videoPingInfo.youtubeId + '</a> ' +
-                '<i>' + new Date(videoPingInfo.dateTimeLastPing).toLocaleString() + '</i></div>');
+doAjax(oneDayAgo, '.one-day-ago', '24 hours');
+doAjax(oneWeekAgo, '.one-week-ago', '7 days');
+
+function doAjax(sinceDateTime, loadingSelector, durationLabel) {
+    var dataContainer = $(loadingSelector).parent();
+    $.ajax({
+        method: 'GET',
+        url: 'api/pings',
+        data: {sinceDateTime: sinceDateTime},
+        dataType: 'json'
+    }).done(function(videoPingInfoList) {
+        $(loadingSelector).remove();
+        if (videoPingInfoList.length) {
+            for (var i = 0; i < videoPingInfoList.length; i++) {
+                var videoPingInfo = videoPingInfoList[i];
+                dataContainer.append(
+                    '<div class="ping">' +
+                    '[' + videoPingInfo.numPings + '] ' +
+                    '<a href="https://www.youtube.com/watch?v=' + videoPingInfo.youtubeId + '" target="_blank">' +
+                    videoPingInfo.youtubeId + '</a> ' +
+                    '<i>' + new Date(videoPingInfo.dateTimeLastPing).toLocaleString() + '</i></div>');
+            }
+        } else {
+            dataContainer.append('No pings in the last ' + durationLabel + '!');
         }
-    } else {
-        $('.pings-data').append('No pings in the last 24 hours!');
-    }
-}).fail(function(jqXHR, textStatus, errorThrown) {
-    $('.pings-data').val(errorThrown);
-});
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        dataContainer.val(errorThrown);
+    });
+}
